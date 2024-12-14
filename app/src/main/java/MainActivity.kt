31f54a684b9ff0cc8147d2a663dcc206be21b4ea
Paul-Franklin-student-programs <pfranklin138@gmail.com>
2024-12-14 15:48:22 -0500

    created list of images in MainActivity to be applied to RecyclerView

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.criticalthinkingfiveimagegallery
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +10,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView: ImageView = findViewById(R.id.imageView)
-        imageView.setImageResource(R.drawable.hr_king_of_town)
+    val charactersList = listOf(
+        R.drawable.hr_bubs,
+        R.drawable.hr_homsar,
+        R.drawable.hr_coach_z,
+        R.drawable.hr_pom_pom,
+        R.drawable.hr_trogdor,
+        R.drawable.hr_homestar,
+        R.drawable.hr_king_of_town,
+        R.drawable.hr_marzipan,
+        R.drawable.hr_strong_bad,
+        R.drawable.hr_strong_mad,
+        R.drawable.hr_strong_sad,
+        R.drawable.hr_the_cheat
+    )
+
+    val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
     }
 }
